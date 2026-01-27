51f8c5e814316bb7de0835ce2ef3dbec03d89d5f
Sam-D-04 <haidang40021181@gmail.com>
2026-01-27 22:22:56 +0700

    2.0

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -47,7 +47,7 @@ const menuItems = [
         />
       </svg>
     ),
-    roles: ['admin'], // Chỉ admin mới thấy
+    roles: ['admin'], 
   },
     {
     name: 'Khách',
@@ -69,21 +69,22 @@ const menuItems = [
         />
       </svg>
     ),
+    
   },
   {
     name: 'Scanner',
-    href: '/scanner', 
-    roles: ['admin', 'security'], 
+    href: '/scanner',
     icon: (
       <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-        <path 
-          strokeLinecap="round" 
-          strokeLinejoin="round" 
-          strokeWidth={2} 
-          d="M12 4v1m6 11h2m-6 0h-2v4m0-11v3m0 0h.01M12 12h4.01M16 20h4M4 12h4m12 0h.01M5 8h2a1 1 0 001-1V5a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1zm12 0h2a1 1 0 001-1V5a1 1 0 00-1-1h-2a1 1 0 00-1 1v2a1 1 0 001 1zM5 20h2a1 1 0 001-1v-2a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1z" 
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M12 4v1m6 11h2m-6 0h-2v4m0-11v3m0 0h.01M12 12h4.01M16 20h4M4 12h4m12 0h.01M5 8h2a1 1 0 001-1V5a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1zm12 0h2a1 1 0 001-1V5a1 1 0 00-1-1h-2a1 1 0 00-1 1v2a1 1 0 001 1zM5 20h2a1 1 0 001-1v-2a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1z"
         />
       </svg>
     ),
+    roles: ['security'],
   },
   {
     name: 'Thẻ truy cập',
@@ -98,6 +99,7 @@ const menuItems = [
         />
       </svg>
     ),
+    roles: ['admin'],
   },
   {
     name: 'Cửa ra vào',
@@ -126,6 +128,7 @@ const menuItems = [
         />
       </svg>
     ),
+    roles: ['admin'],
   },
    {
     name: 'Phân quyền',
